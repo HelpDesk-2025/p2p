9b8b9be383974e22819c341dab41d36953c891c6
HelpDesk-2025 <helpdesk@stjoseph-group.com>
2026-01-07 11:01:52 +0800

    Updated ApprovalProgressTracker.tsx

--- src/components/ApprovalProgressTracker.tsx
+++ src/components/ApprovalProgressTracker.tsx
@@ -407,8 +407,9 @@ export function ApprovalProgressTracker({
             <span className="font-semibold">Request Fully Approved</span>
           </div>
 
-          {/* Hide MSBC sync for Purchase Order type PRs */}
-          {!((requestType === 'Purchase Requisition' || requestType === 'purchase_requisition') && purchaseType === 'Purchase Order') && (
+          {/* Hide MSBC sync for Purchase Order type PRs and Petty Cash */}
+          {!((requestType === 'Purchase Requisition' || requestType === 'purchase_requisition') && purchaseType === 'Purchase Order') &&
+           !(requestType === 'Petty Cash' || requestType === 'petty_cash') && (
             <div className="relative">
               <div className="flex items-start gap-4">
                 <div className="flex-shrink-0 z-10">
